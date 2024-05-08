77ef0a2f47bc86145ce0feee41e5046c70c84c73
Oleksandr Fomin <oleksandr.fomin@inoxoft.com>
2024-05-08 21:22:31 +0300

    changed google fonts to locally hosted fonts

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+source .env
+
 docker volume rm tt-logs 
 #remove the `-it` flag and add `-d` if you are running via a timer/cron job
-docker run --rm -it -e ANTHROPIC_API_KEY=$ANTHROPIC_API_KEY -e OPENAI_API_KEY=$OPENAI_API_KEY -e MISTRAL_API_KEY=$MISTRAL_API_KEY -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e NOTDIAMOND_API_KEY=$NOTDIAMOND_API_KEY -e GOOGLE_API_KEY=$GOOGLE_API_KEY -e TOGETHER_API_KEY=$TOGETHER_API_KEY -e COHERE_API_KEY=$COHERE_API_KEY -e DEBUG=true -v $(pwd)/debug:/usr/src/app/debug -v tt-logs:/var/log/tt --ulimit nofile=32768 ghcr.io/medusa-ml/translatetribune:latest
+docker run --rm -it -e ANTHROPIC_API_KEY=$ANTHROPIC_API_KEY -e OPENAI_API_KEY=$OPENAI_API_KEY -e MISTRAL_API_KEY=$MISTRAL_API_KEY -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e NOTDIAMOND_API_KEY=$NOTDIAMOND_API_KEY -e GOOGLE_API_KEY=$GOOGLE_API_KEY -e TOGETHER_API_KEY=$TOGETHER_API_KEY -e COHERE_API_KEY=$COHERE_API_KEY -e DEBUG=true -v C:/Users/Alexa/OneDrive/Desktop/tt/TranslateTribune/debug:/usr/src/app/debug -v tt-logs:/var/log/tt --ulimit nofile=32768 ghcr.io/medusa-ml/translatetribune:latest
 
