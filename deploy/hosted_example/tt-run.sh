3a250d0b8e519237a405865b7585003886287aa2
Brad Flaugher <brad@bradflaugher.com>
2025-01-29 06:36:06 -0500

    chrome shared memory

--- deploy/hosted_example/tt-run.sh
+++ deploy/hosted_example/tt-run.sh
@@ -5,5 +5,5 @@ docker container prune -f
 if docker volume ls -q | grep -q "^tt-logs$"; then
     docker volume rm tt-logs
 fi
-docker run -d -e ANTHROPIC_API_KEY="ENTER_API_KEY_HERE" -e OPENAI_API_KEY="ENTER_API_KEY_HERE" -e MISTRAL_API_KEY="ENTER_API_KEY_HERE" -e GOOGLE_API_KEY="ENTER_API_KEY_HERE" -e OPENROUTER_API_KEY="ENTER_API_KEY_HERE" -e AWS_ACCESS_KEY_ID="ENTER_API_KEY_HERE" -e AWS_SECRET_ACCESS_KEY="ENTER_API_KEY_HERE" -v tt-logs:/var/log/tt --ulimit nofile=32768 ghcr.io/medusa-intelligence-corp/translatetribune:latest
+docker run --shm-size=3g -d -e ANTHROPIC_API_KEY="ENTER_API_KEY_HERE" -e OPENAI_API_KEY="ENTER_API_KEY_HERE" -e MISTRAL_API_KEY="ENTER_API_KEY_HERE" -e GOOGLE_API_KEY="ENTER_API_KEY_HERE" -e OPENROUTER_API_KEY="ENTER_API_KEY_HERE" -e AWS_ACCESS_KEY_ID="ENTER_API_KEY_HERE" -e AWS_SECRET_ACCESS_KEY="ENTER_API_KEY_HERE" -v tt-logs:/var/log/tt --ulimit nofile=32768 ghcr.io/medusa-intelligence-corp/translatetribune:latest
 
--- utils/browser.py
+++ utils/browser.py
@@ -3,6 +3,8 @@ import logging
 
 import requests
 
+from tempfile import mkdtemp
+
 from urllib import robotparser
 from urllib.parse import urlparse
 
@@ -43,12 +45,17 @@ def setup_driver():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument('--headless=new')  # for newer Chrome versions
 
     # Additional recommended options
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--disable-infobars")
     chrome_options.add_argument("--window-size=1920,1080")
+    
+    #create user directory for each session
+    user_data_dir = mkdtemp()
+    chrome_options.add_argument(f'--user-data-dir={user_data_dir}')
 
     # Set timeouts in capabilities
     chrome_options.set_capability("pageLoadStrategy", "normal")
